30333b16588db1d186d6aa6949c6247ccf08d33f
PROFW52 <PROFW52@lab.aca>
2016-10-03 08:32:46 -0500

    Patron Adapter implementado

---
+++ src/java/pe/edu/ulima/visortexto/ModoVisualizacionAdapter.java
@@ -0,0 +1,10 @@
+
+package pe.edu.ulima.visortexto;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public interface ModoVisualizacionAdapter {
+    public ByteArrayOutputStream renderizar(
+            String titulo, String contenido) throws IOException;
+}
